f8be2aeab7769c9f88599f81b14b6b731648489c
IvanBayandin <v.bayandin2016@outlook.com>
2024-11-11 17:26:32 +0500

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,19 @@
+from sys import is_stack_trampoline_active
+
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+
+while index < len(my_list):
+    number = my_list[index]
+    index = index + 1
+    if number == 0:
+        continue # пропускаем 0
+    elif number < 0:
+        print('Встретилось отрицательное число:', number)
+        break
+    elif index == len(my_list):
+        print(number)
+        print('Список закончился')
+    else:
+        print (number)
+
